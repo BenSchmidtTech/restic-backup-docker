20d282da02c58dabf9e308dfcd56cb30577f38ed
Ben Schmidt <benschmidt@benschmidt.tech>
2020-12-18 23:43:13 -0700

    Push Notifications via APPRISEURL

---
+++ post-backup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+title="Restic Backup Report"
+body="$(cat /var/log/backup-last.log)"
+
+json=$( jq -n \
+    --arg bdy "$body" \
+    --arg ttl "$title" \
+    '{body: $bdy, title: $ttl}' )
+
+contentType="Content-Type: application/json"
+
+curl -X POST -d "$json" -H $contentType "$APPRISEURL"
+
